49af73ae5cc504994e607554f394d2b671329c24
rafiazharr17 <rafipilo17@gmail.com>
2023-06-21 15:31:12 +0700

    menambahkan ".txt"

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -33,7 +33,7 @@ int main() {
 	//membuka file dalam mode membaca
 	ifstream infile;
 	//menunjuk ke sebuah file
-	infile.open(NamaFile + ".txt", ios::in); 
+	infile.open(NamaFile, ios::in);  
 
 	cout << endl << ">= Membuka dan membaca file " << endl;
 	//jika ada file maka
