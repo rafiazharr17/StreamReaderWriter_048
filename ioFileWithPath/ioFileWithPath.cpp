f653d127e7659d0f7a41b4a920f4b3146a08c364
rafiazharr17 <rafipilo17@gmail.com>
2023-06-21 15:28:51 +0700

    menghapus ".txt"

--- ioFileWithPath/ioFileWithPath.cpp
+++ ioFileWithPath/ioFileWithPath.cpp
@@ -33,7 +33,7 @@ int main() {
 	//membuka file dalam mode membaca
 	ifstream infile;
 	//menunjuk ke sebuah file
-	infile.open(NamaFile + ".txt"); 
+	infile.open(NamaFile + ".txt", ios::in); 
 
 	cout << endl << ">= Membuka dan membaca file " << endl;
 	//jika ada file maka
