ec67ca0e03cca898a93bdfda2a5b829320c0f6d7
rafiazharr17 <rafipilo17@gmail.com>
2023-06-21 15:35:57 +0700

    membuat manual exeption

--- manualExeption/manualExeption.cpp
+++ manualExeption/manualExeption.cpp
@@ -1,3 +1,20 @@
 #include <iostream>
 using namespace std;
 
+int main() {
+	try {
+		cout << "Selamat Belajar di Prodi TI UMY" << endl;
+		throw 0.5; //melemparkan sebuah integer maka
+		cout << "Pernyataan tidak akan dieksekusi" << endl;
+	}
+	catch (int a) {
+		//blok ini akan dieksekusi
+		cout << "Pengecualian akan dieksekusi" << endl;
+	}
+	catch (...) {
+		//jika selain integer maka blok ini akan dieksekusi
+		cout << "Default Pengecualian dieksekusi" << endl;
+	}
+
+	return 0;
+}
